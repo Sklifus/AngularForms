a4c5cd1b503f03802e3d6dde81afee43524a40cc
Sklifus <61739733+Sklifus@users.noreply.github.com>
2022-12-29 12:17:37 +0300

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+/* import { FormControl,FormGroup, Validators } from '@angular/forms' */
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularForms';
+  
+  titlesItems = [
+    'Техподдержка',
+    'Продажи',
+    'Другое',
+    'Еще один пункт'
+  ];
+
+  submitForm(formName: NgForm){
+
+  }
+  /* myForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    tel: new FormControl('', [Validators.required, Validators.pattern(/^[0-9]+$/)]),
+    topic: new FormControl(''),
+    message: new FormControl('')
+  })
+
+  get name() { return this.myForm.get('name'); }
+  get email() { return this.myForm.get('email'); }
+  get tel() { return this.myForm.get('tel'); } */
+
+  /* submitForm(){
+    console.log(this.myForm);
+  } */
 }
